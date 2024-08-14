263c68af346a0d0b80a642d00dd955916d7339fd
Anastasiyatarazevich <anastasiya.tarazevich22@gmail.com>
2024-08-14 19:12:34 +0300

    Behavior added

--- app/src/main/java/com/example/s_app/MainActivity.kt
+++ app/src/main/java/com/example/s_app/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.s_app
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.Manifest
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myTextView: TextView
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        myTextView = findViewById(R.id.myTextView)
+        initText(myTextView)
     }
+
+    fun initText(textView: TextView) {
+        val textView: TextView? = null
+        textView!!.text = R.string.user_name.toString()
+    }
+
+
 }
