d153d569eadb964fbbb3d58386d8ad0d869c902a
Anastasiyatarazevich <anastasiya.tarazevich22@gmail.com>
2024-08-15 17:30:56 +0300

    Permissions and logic added

--- app/src/main/java/com/example/s_app/MainActivity.kt
+++ app/src/main/java/com/example/s_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.s_app
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val repository = Repository()
+
+        try {
+            val data = repository.getData()
+            Toast.makeText(this, "Data: $data", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+}
+
+data class DataModel(val id: Int, val value: String)
+
+class Repository {
+
+    private val dataModel = DataModel(1, "Sample Data")
+
+    private fun getData(): DataModel {
+        return dataModel
     }
 }
